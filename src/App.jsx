b094f8a3c764b717e7db524b31427d249c592f53
Carlos Wosiak <carlosw403@hotmail.com>
2021-03-10 21:07:31 -0300

    initial jsx project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
+import BlogPosts from './components/BlogPosts/BlogPosts';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BlogPosts />
+    </div>
+  );
+}
+
+export default App;
